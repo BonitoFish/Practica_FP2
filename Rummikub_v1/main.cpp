9e65a7e51d70cef930a91bf9d1e3351eef90171e
BonitoFish <99401370+BonitoFish@users.noreply.github.com>
2022-03-12 20:57:27 +0100

    Refactorizado algunos métodos e implementado algunas funciones

--- Rummikub_v1/main.cpp
+++ Rummikub_v1/main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-<<<<<<< HEAD
 #include "main.h"
 
 using namespace std;
 
 int main() {
-
-	std::srand(time(NULL));
-
-	for (int i = 0; i < 100; i++) {
-		cout << std::rand() % 8 + 0 << endl;
-	}
 	
 	return 0;
 }
@@ -24,9 +17,14 @@ int menu() {
 	return op;
 }
 
-void inicializarBolsa(tBolsa& bolsa) {
-	/* TODO, se hay que iniciar los valores de las fichas de la bolsa, pero no se si eso se hará
-	con un fichero de entrada o de alguna otra manera*/
+void inicializarBolsa(tBolsa& bolsa) {	
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < bolsa.num_fichas; j++) {
+			bolsa.fichas[i][j].valor = j + 1;
+			bolsa.fichas[i][j].color = (i == 0 || i == 4) ? rojo : (i == 1 || i == 5) ? verde :
+				(i == 2 || i == 6) ? azul : amarillo;
+		}
+	}
 }
 
 tFicha robar(tBolsa& bolsa) {
@@ -39,6 +37,7 @@ tFicha robar(tBolsa& bolsa) {
 	if (bolsa.fichas[fila_rand][col_rand].color != libre) {
 		ret = { bolsa.fichas[fila_rand][col_rand].valor, bolsa.fichas[fila_rand][col_rand].color };
 		bolsa.fichas[fila_rand][col_rand] = { -1, libre };
+		return ret;
 	}
 	else {
 		for (int i = fila_rand; i < 8; i++) {
@@ -46,38 +45,147 @@ tFicha robar(tBolsa& bolsa) {
 				if (bolsa.fichas[i][j].color != libre) {
 					ret = { bolsa.fichas[i][j].valor, bolsa.fichas[i][j].color };
 					bolsa.fichas[i][j] = { -1, libre };
-					break;
+					return ret;
 				}
 			}
 		}
 
-		if (ret.color == libre) {
-			for (int i = 0; i < fila_rand; i++) {
-				for (int j = 0 ; j < bolsa.num_fichas; j++) {
-					if (bolsa.fichas[i][j].color != libre) {
-						ret = { bolsa.fichas[i][j].valor, bolsa.fichas[i][j].color };
-						bolsa.fichas[i][j] = { -1, libre };
-						break;
-					}
-				}
-			}
-		}
-
-		if (ret.color == libre) {
-			for (int i = fila_rand;;) {
-				for (int j = 0; j < col_rand; j++) {
-					if (bolsa.fichas[fila_rand][col_rand].color != libre) {
-						ret = { bolsa.fichas[i][j].valor, bolsa.fichas[i][j].color };
-						bolsa.fichas[i][j] = { -1, libre };
-						break;
-					}
+		for (int i = 0; i <= fila_rand; i++) {
+			for (int j = 0; j < bolsa.num_fichas; j++) {
+				if (bolsa.fichas[i][j].color != libre) {
+					ret = { bolsa.fichas[i][j].valor, bolsa.fichas[i][j].color };
+					bolsa.fichas[i][j] = { -1, libre };
+					return ret;
 				}
 			}
 		}
 	}
-
 	return ret;
 }
+
+
+void repartir(tBolsa& bolsa, tSoportes& soportes) {
+	for (int i = 0; i < soportes.num_jug; i++) {
+		for (int j = 0; j < bolsa.num_fichas; j++) {
+			soportes.soporte_jug[i].lista_ficha[j] = robar(bolsa);
+			soportes.soporte_jug[i].num_fichas++;
+		}
+	}
+}
+
+
+void ordenarPorNum(tSoporte& soporte) {
+	tFicha tmp;
+	for (int i = 0; i < soporte.num_fichas - 1; i++) {
+		for (int j = soporte.num_fichas - 1; j > i; j--) {
+			if (soporte.lista_ficha[j].valor < soporte.lista_ficha[j - 1].valor) {
+				tmp = soporte.lista_ficha[j];
+				soporte.lista_ficha[j] = soporte.lista_ficha[j - 1];
+				soporte.lista_ficha[j - 1] = tmp;
+			}
+		}
+	}
+}
+
+void ordenarPorColor(tSoporte& soporte) {
+
+
+
+}
+
+
+bool operator==(tFicha izq, tFicha der) {
+	return (izq.color == der.color && izq.valor == der.valor);
+}
+
+
+int buscar(const tJugada jugada, tFicha ficha) {
+	int ret = -1;
+	int i = 0;
+
+	while (i < jugada.num_fichas_jugadas && ret == -1 ) {
+		if (jugada.lista_jugada[i] == ficha) {
+			ret = i;
+		}
+		++i;
+	}
+	return ret;
+}
+
+
+void mostrarSeries(tSoporte soporte) {
+	tFicha serie[4];
+
+
+}
+
+void mostrarEscaleras(tSoporte soporte) {
+
+
+}
+
+
+void iniJugada(tJugada &jugada) {
+	for (int i = 0; i < jugada.num_fichas; i++) {
+		jugada.lista_jugada[i] = { -1, libre };
+	}
+}
+
+void nuevaJugada(tTablero& tablero, tJugada jugada) {
+	if (tablero.cont_jugadas < tablero.num_fichas * 2) {
+		tablero.lista_jugadas[tablero.cont_jugadas] = jugada;
+		tablero.cont_jugadas++;
+	}
+	else {
+		cout << "\t\t[Limite de jugadas posibles]" << endl;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void parametros(int& numero_j, int& numero_fichas) {
 	char aux;
@@ -109,13 +217,6 @@ void parametros(int& numero_j, int& numero_fichas) {
 
 	cout << endl;
 }
-=======
-using namespace std;
 
 
-int main () {
-cout << "prueba";
 
-
-}
->>>>>>> 17b6d2eae08592080ecce8e07267c9a764c1892a
--- Rummikub_v1/main.h
+++ Rummikub_v1/main.h
@@ -3,10 +3,13 @@
 // Variables globales
 const int MIN_NumJugadores = 2;
 const int MAX_NumJugadores = 4;
+
 const int MIN_NumFichas = 8;
 const int MAX_NumFichas = 13;
+
 const int MIN_IniFichas = 6;
 const int MAX_IniFichas = 14;
+
 const int MaxJugadas = MAX_NumFichas * 2;
 const int MaxFichas = 50;
 
@@ -20,34 +23,63 @@ typedef struct {
 
 // El soporte representa las fichas que tiene un jugador
 typedef struct {
-	tFicha lista_ficha[MAX_IniFichas*2];
-	int num_fichas;
+	tFicha lista_ficha[MaxFichas];
+	int num_fichas;	
 }tSoporte;
 
-tSoporte tSoportes[MAX_NumJugadores];
+// Estructura que mantiene los soportes de los jugadores
+typedef struct {
+	tSoporte soporte_jug[MAX_NumJugadores];
+	int num_jug;
+}tSoportes;
 
 // La bolsa contiene las fichas a repartir y de donde se tiene que robar 
 typedef struct {
 	tFicha fichas[8][MAX_NumFichas];
-	int num_fichas;
-	int fichas_disponibles;
+	int num_fichas;	// Representa el tamaño de la columna del array bidimensional
+	int fichas_disponibles;		
 }tBolsa;
 
+// tJugada representa cada jugada que realiza el jugadores, además termina con el centinela -1
 typedef tFicha tJugada[MAX_NumFichas + 1];
+
+typedef struct {
+	tFicha lista_jugada[MAX_NumFichas + 1];
+	int num_fichas;		// Número máximo de fichas de la jugada
+	int num_fichas_jugadas;		// Número de fichas jugadas
+}tJugada;
 
 // El tablero es donde contiene las jugadas 
 typedef struct {
 	tJugada lista_jugadas[MaxJugadas];
+	int num_fichas;
 	int cont_jugadas;
 }tTablero;
 
 
 // Funciones 
-int menu();	// Gestiona las opciones que puede seleccionar cada usuario
+int menu();		// Gestiona las opciones que puede seleccionar cada usuario
 
 void inicializarBolsa(tBolsa& bolsa);	// Inicializa la bolsa con las fichas	
 
 tFicha robar(tBolsa& bolsa);	// Devuelve una ficha obtenida de la bolsa
 
+void repartir(tBolsa& bolsa, tSoportes soportes);	// Obtiene IniFichas para cada jugador
+
+void ordenarPorNum(tSoporte& soporte);		// Ordena las fichas del soporte por números, de menor a mayor.
+
+void ordenarPorColor(tSoporte& soporte);	// Ordena las fichas por colores y luego por cada calor ordenar por valor
+
+bool operator==(tFicha izq, tFicha der);	// Devuelve true si dos fichas son iguales a color y valor
+
+int buscar(const tJugada jugada, tFicha ficha);		// Devuelve la posición de la ficha en la jugada
+
+void mostrarSeries(tSoporte soporte);	// Muestran las posíbles series que se puedan crear con las fichas del soporte
+
+void mostrarEscaleras(tSoporte soporte);	// Muestran las posíbles escaleraas que se puedan crear con las fichas del soporte
+
+void iniJugada(tJugada jugada);		// Inicia las fichas de la jugada a -1
+
+void nuevaJugada(tTablero& tablero, tJugada jugada);	// Añade al tablero la nueva jugada
 
 void parametros(int& numero_jugador, int& numero_fichas);
