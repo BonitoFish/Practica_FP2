dea1c31b7d9c39d3954aed3b7772fd08236dd989
BonitoFish <99401370+BonitoFish@users.noreply.github.com>
2022-03-19 19:46:35 +0100

    Hecha y comprobada las funciones mostrarSeries y mostrarEscaleras y algunas cositas más

--- Rummikub_v1/main.cpp
+++ Rummikub_v1/main.cpp
@@ -5,8 +5,37 @@ using namespace std;
 
 int main() {
 	
+	tSoporte lista;
+	lista.lista_ficha[0] = { 7, azul };
+	lista.lista_ficha[1] = { 3, azul };
+	lista.lista_ficha[2] = { 9, amarillo };
+	lista.lista_ficha[3] = { 6, azul };
+	lista.lista_ficha[4] = { 5, azul };
+	lista.lista_ficha[5] = { 6, verde };
+	lista.lista_ficha[6] = { 9, rojo };
+	lista.lista_ficha[7] = { 7, rojo };
+	lista.lista_ficha[8] = { 1, verde };
+	lista.lista_ficha[9] = { 2, verde };
+	lista.lista_ficha[10] = { 6, amarillo };
+	lista.lista_ficha[11] = { 6, rojo };
+	lista.lista_ficha[12] = { 7, verde };
+	lista.num_fichas = 13;
+
+
+	ordenarPorNum(lista);
+	ordenarPorColor(lista);
+	
+	mostrarSeries(lista);
+	mostrarEscaleras(lista);
+	
+
+	colorTexto(blanco);
+	
+	
+
 	return 0;
 }
+
 
 int menu() {
 	int op;
@@ -17,6 +46,7 @@ int menu() {
 	return op;
 }
 
+
 void inicializarBolsa(tBolsa& bolsa) {	
 	for (int i = 0; i < 8; i++) {
 		for (int j = 0; j < bolsa.num_fichas; j++) {
@@ -26,6 +56,7 @@ void inicializarBolsa(tBolsa& bolsa) {
 		}
 	}
 }
+
 
 tFicha robar(tBolsa& bolsa) {
 	tFicha ret = { -1, libre };
@@ -87,10 +118,18 @@ void ordenarPorNum(tSoporte& soporte) {
 	}
 }
 
+
 void ordenarPorColor(tSoporte& soporte) {
-
-
-
+	tFicha tmp;
+	for (int i = 0; i < soporte.num_fichas - 1; i++) {
+		for (int j = soporte.num_fichas - 1; j > i; j--) {
+			if (soporte.lista_ficha[j].color < soporte.lista_ficha[j - 1].color) {
+				tmp = soporte.lista_ficha[j];
+				soporte.lista_ficha[j] = soporte.lista_ficha[j - 1];
+				soporte.lista_ficha[j - 1] = tmp;
+			}
+		}
+	}
 }
 
 
@@ -103,7 +142,7 @@ int buscar(const tJugada jugada, tFicha ficha) {
 	int ret = -1;
 	int i = 0;
 
-	while (i < jugada.num_fichas_jugadas && ret == -1 ) {
+	while (i < jugada.num_fichas_jugada && ret == -1 ) {
 		if (jugada.lista_jugada[i] == ficha) {
 			ret = i;
 		}
@@ -114,14 +153,105 @@ int buscar(const tJugada jugada, tFicha ficha) {
 
 
 void mostrarSeries(tSoporte soporte) {
-	tFicha serie[4];
+	int num[13];	// Lista números que ya se ha escogido para no repetir
+	int tam_num = 0;
 
+	// Recorrido de las fichas del soporte
+	for (int i = 0; i < soporte.num_fichas; i++) {
 
+		tFicha serie[4];	// La serie que voy a devolver al final si existe
+		int tam_serie = 0;
+
+		if (tam_num == 0) {	// Este es primer caso para un primer valor (solo se evalúa una vez)
+			num[0] = soporte.lista_ficha[i].valor;
+			tam_num++;	
+		}
+		else {
+			bool duplicado = false;
+			for (int j = 0; j < tam_num; j++) {	
+				if (num[j] == soporte.lista_ficha[i].valor) {
+					duplicado = true;
+				}
+			}
+
+			if (duplicado) {	// Si se trata de un número ya recorrido anteriormente se pasa a la siguiente iteración
+				continue;
+			}
+			else {	// De lo contrario guardamos ese valor para no repetir en las siguientes iteraciones
+				num[tam_num] = soporte.lista_ficha[i].valor;
+				tam_num++;
+			}
+		}
+
+		serie[0] = soporte.lista_ficha[i];	// Suponemos que el primer elemento siempre será el del índice i
+		tam_serie++;
+
+		for (int j = i + 1; j < soporte.num_fichas; j++) {	// Recorro para buscar elementos de la serie a devolver
+
+			if (soporte.lista_ficha[j].valor == serie[0].valor ) {
+				bool mismo_color = false;
+				for (int k = 0; k < tam_serie; k++) {
+					if (serie[k].color == soporte.lista_ficha[j].color) {
+						mismo_color = true;
+					}
+				}
+
+				if (mismo_color) {	// Lo mismo que arriba, se comprueba si tiene el mismo color
+					continue;
+				}
+				else {	// De lo contrario se introduce en la serie a devolver
+					serie[tam_serie] = soporte.lista_ficha[j];
+					tam_serie++;
+				}
+			}
+
+		}
+
+		// Devuelve la serie si supera el tamaño mínimo
+		if (tam_serie >= 3) {
+			for (int i = 0; i < tam_serie; i++) {
+				colorTexto(serie[i].color);
+				cout << serie[i].valor << " ";
+			}
+			colorTexto(blanco);
+			cout << endl;
+		}
+	}
 }
+
 
 void mostrarEscaleras(tSoporte soporte) {
 
+	for (int i = 0; i < soporte.num_fichas; i++) {	// Se itera sobre cada uno de los elementos del soporte
 
+		tFicha escalera[13];	// La escalera a devolver si existe
+		escalera[0] = soporte.lista_ficha[i];	// Se supone que en cada iteración el primer elem es el del índice i
+		int tam_escalera = 1;
+
+		tFicha elem = soporte.lista_ficha[i];	// Variable que utilizaremos como auxiliar
+
+		int k = i;
+		int j = (i + 1) % soporte.num_fichas;
+		while (j != k) {	// Recorrido circular 
+			if (soporte.lista_ficha[j].valor == elem.valor + 1 && soporte.lista_ficha[j].color == elem.color) {
+				escalera[tam_escalera] = soporte.lista_ficha[j];
+				tam_escalera++;
+				elem = soporte.lista_ficha[j];	// Si existe elemento consecutivo empezar la búsqueda desde allí
+				k = j;
+			}
+			j = (j + 1) % soporte.num_fichas;
+		}
+
+		// Igual que en mostrarSerie()
+		if (tam_escalera >= 3) {
+			for (int i = 0; i < tam_escalera; i++) {
+				colorTexto(escalera[i].color);
+				cout << escalera[i].valor << " ";
+			}
+			colorTexto(blanco);
+			cout << endl;
+		}
+	}
 }
 
 
@@ -130,6 +260,7 @@ void iniJugada(tJugada &jugada) {
 		jugada.lista_jugada[i] = { -1, libre };
 	}
 }
+
 
 void nuevaJugada(tTablero& tablero, tJugada jugada) {
 	if (tablero.cont_jugadas < tablero.num_fichas * 2) {
@@ -140,6 +271,54 @@ void nuevaJugada(tTablero& tablero, tJugada jugada) {
 		cout << "\t\t[Limite de jugadas posibles]" << endl;
 	}
 }
+
+
+void eliminaFichas(tSoporte& soporte, const tJugada jugada) {
+
+	int i = 0;
+	while (jugada.lista_jugada[i].valor != -1) {
+		for (int j = 0; j < soporte.num_fichas; j++) {
+			if (soporte.lista_ficha[j] == jugada.lista_jugada[i]) {
+
+				for (int k = j; k < soporte.num_fichas-1; k++) {
+					soporte.lista_ficha[k] = soporte.lista_ficha[k + 1];
+				}
+				soporte.lista_ficha[soporte.num_fichas] = { -1, libre };
+				--soporte.num_fichas;
+			}
+		}
+		++i;
+	}
+}
+
+
+int nuevaJugada(tSoporte soporte, tJugada jugada) {
+
+
+
+}
+
+
+void colorTexto(tColor color) {
+	switch (color) {
+	case amarillo:
+		cout << "\033[1;40;33m";
+		break;
+	case azul:
+		cout << "\033[40;34m";
+		break;
+	case rojo:
+		cout << "\033[40;31m";
+		break;
+	case verde:
+		cout << "\033[40;32m";
+		break;
+	case blanco:
+		cout << "\033[40;37m";
+		break;
+	}
+}
+
 
 
 
--- Rummikub_v1/main.h
+++ Rummikub_v1/main.h
@@ -24,7 +24,7 @@ typedef struct {
 // El soporte representa las fichas que tiene un jugador
 typedef struct {
 	tFicha lista_ficha[MaxFichas];
-	int num_fichas;	
+	int num_fichas;		// Número de fichas del soporte
 }tSoporte;
 
 // Estructura que mantiene los soportes de los jugadores
@@ -40,13 +40,10 @@ typedef struct {
 	int fichas_disponibles;		
 }tBolsa;
 
-// tJugada representa cada jugada que realiza el jugadores, además termina con el centinela -1
-typedef tFicha tJugada[MAX_NumFichas + 1];
-
 typedef struct {
 	tFicha lista_jugada[MAX_NumFichas + 1];
 	int num_fichas;		// Número máximo de fichas de la jugada
-	int num_fichas_jugadas;		// Número de fichas jugadas
+	int num_fichas_jugada;		// Número de fichas jugadas
 }tJugada;
 
 // El tablero es donde contiene las jugadas 
@@ -78,8 +75,14 @@ void mostrarSeries(tSoporte soporte);	// Muestran las posíbles series que se pue
 
 void mostrarEscaleras(tSoporte soporte);	// Muestran las posíbles escaleraas que se puedan crear con las fichas del soporte
 
-void iniJugada(tJugada jugada);		// Inicia las fichas de la jugada a -1
+void iniJugada(tJugada &jugada);		// Inicia las fichas de la jugada a -1
 
 void nuevaJugada(tTablero& tablero, tJugada jugada);	// Añade al tablero la nueva jugada
 
+void eliminaFichas(tSoporte& soporte, const tJugada jugada); // Elimina del soporte las fichas que hay en la jugada.
+
+int nuevaJugada(tSoporte soporte, tJugada jugada); // Permite al usuario crear una jugada con fichas que haya en su soporte.
+
 void parametros(int& numero_jugador, int& numero_fichas);
+
+void colorTexto(tColor color);
